5f0b2a8d9beee328bd22a054bc624cc488f0b475
jim <jim@supersweet.dev>
2021-11-18 19:35:21 -0600

    initial string processing

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def logger():
+    return "You're talking to Care-Classifier"
+@app.route('/process', methods=["POST"])
+def process():
+    inputString = request.get_json(force=True)['text']
+    allLower = inputString.lower()
+    allUpper = inputString.upper()
+    return jsonify({'lower case': allLower, 'upper case': allUpper})
