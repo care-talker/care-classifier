dfbaf16f81d633bb1b0370b7c0090c7c6d44bd67
supersweet-dev <pentagr4mi@gmail.com>
2021-11-30 15:44:37 -0600

    preliminary emotional classification

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, request, jsonify
+from classifier import classify
 app = Flask(__name__)
 
 
 @app.route('/')
 def logger():
     return "You're talking to Care-Classifier"
-@app.route('/process', methods=["POST"])
-def process():
+@app.route('/test', methods=["POST"])
+def test():
     inputString = request.get_json(force=True)['text']
     allLower = inputString.lower()
     allUpper = inputString.upper()
     return jsonify({'original': inputString, 'lower case': allLower, 'upper case': allUpper})
+@app.route('/classify', methods=["POST"])
+def process():
+    return jsonify(classify(request.get_json(force=True)['text']))
